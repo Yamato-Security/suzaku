d5733ef07a2ed6ad63100807c7bbe574b37be09e
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-03-02 16:39:35 +0900

    add cli

---
+++ src/cmd.rs
@@ -0,0 +1,38 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "Suzaku")]
+#[command(version = "0.0.1")]
+#[command(author = "Yamato Security @SecurityYamato")]
+#[command(about = "CloudTrail Threat Detection")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub(crate) cmd: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    #[command(
+        about = "Scans AWS CloudTrail logs using Sigma rules to detect potential security threats."
+    )]
+    AwsDetect {
+        #[arg(
+            short,
+            long,
+            value_name = "DIR",
+            help = "Directory of multiple log files"
+        )]
+        directory: Option<String>,
+
+        #[arg(
+            short,
+            long,
+            value_name = "FILE",
+            help = "The log file to scan(json/gz)"
+        )]
+        file: Option<String>,
+
+        #[arg(short, long, value_name = "FILE", help = "Output JSON")]
+        output: Option<String>,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use crate::cmd::{Cli, Commands};
+use clap::Parser;
+use std::fs;
+
+mod cmd;
+
 fn main() {
-    println!("Hello, world!");
+    let logo = fs::read_to_string("art/logo.txt").expect("Failed to read logo file");
+    println!("\x1b[38;2;255;175;0m{}\x1b[0m", logo);
+    println!();
+
+    let cli = Cli::parse();
+    match &cli.cmd {
+        Commands::AwsDetect {
+            directory,
+            file,
+            output,
+        } => {
+            if let Some(dir) = directory {
+                println!("Directory: {}", dir);
+            }
+            if let Some(file) = file {
+                println!("File: {}", file);
+            }
+            if let Some(output) = output {
+                println!("Output: {}", output);
+            }
+        }
+    }
 }
