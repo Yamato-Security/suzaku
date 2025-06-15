ae79cb23445bf5485b9e755931b0f0eefa63e0ef
hach1yon <you@example.com>
2025-06-15 14:23:28 +0900

    fix clippy error

--- src/cmd/aws_detect.rs
+++ src/cmd/aws_detect.rs
@@ -383,10 +383,11 @@ pub fn aws_detect(options: &AwsCtTimelineOptions, common_opt: &CommonOptions) {
         )
         .unwrap();
     } else if let Some(f) = &options.input_opt.filepath {
+        let ref_rules:&Vec<Rule> = rules.as_ref();
         scan_file(
             f,
             options,
-            rules.as_ref(),
+            ref_rules,
             &mut summary,
             &profile,
             &mut wrt,
