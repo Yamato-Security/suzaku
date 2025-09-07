0b6925eeeff9cd46ca23c19e138e32b86d6d3bdd
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-09-08 01:39:42 +0900

    feat: add profile existence check in AWS and Azure timeline functions

--- src/cmd/aws/aws_timeline.rs
+++ src/cmd/aws/aws_timeline.rs
@@ -1,7 +1,19 @@
 use crate::core::log_source::LogSource;
 use crate::core::timeline::make_timeline;
+use crate::core::util::p;
 use crate::option::cli::{CommonOptions, TimelineOptions};
+use std::path::Path;
 
 pub fn aws_timeline(options: &TimelineOptions, common_opt: &CommonOptions) {
-    make_timeline(options, common_opt, LogSource::Aws);
+    let log = LogSource::Aws;
+    let profile_path = log.profile_path();
+    if !Path::new(profile_path).exists() {
+        p(
+            None,
+            &format!("Profile file does not exist: {:?}", profile_path),
+            true,
+        );
+        return;
+    }
+    make_timeline(options, common_opt, log);
 }
