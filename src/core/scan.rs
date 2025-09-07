d28e78c392480af44d40946914c6daceface4c51
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-09-08 01:05:04 +0900

    refactor: improve readability of timestamp key handling in scan.rs

--- src/core/scan.rs
+++ src/core/scan.rs
@@ -205,7 +205,10 @@ fn detect_events<'a>(
     // If all the events are loaded at once, it can consume too much memory.
     // To avoid the problem, we split the events into chunks.
     const CHUNK_SIZE: usize = 1000;
-    let ts_key = context.prof_ts_key.strip_prefix(".").unwrap_or(context.prof_ts_key.as_str());
+    let ts_key = context
+        .prof_ts_key
+        .strip_prefix(".")
+        .unwrap_or(context.prof_ts_key);
     for event_chunks in events.chunks(CHUNK_SIZE) {
         // Convert loaded events into JSON
         // I call the collect() function at the end of this block due to a lifetime issue of json_event.
